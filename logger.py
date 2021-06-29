f9f8d7cfeeebbc2c961aec79805c32c31b413f13
Elon Gliksberg <elongliks@gmail.com>
2021-06-29 14:12:37 +0300

    Added level to the logger.

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('alakazam')
 
 def init_logger():
     logger.setLevel(logging.DEBUG)
-    log_formatter = logging.Formatter('%(asctime)s %(message)s', '%d/%m/%Y %H:%M:%S')
+    log_formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s', '%d/%m/%Y %H:%M:%S')
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
